2f0e76324889879ec58903f03baefa7892aa1917
dhog10 <dhog10@googlemail.com>
2020-10-03 13:20:57 +0100

    stuff

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public static Generator Instance;
+
+    public TaskIndicator[] m_Tasks;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TaskIndicator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskIndicator : MonoBehaviour
+{
+    public Material m_CompleteMaterial;
+    public Material m_IncompleteMaterial;
+    public MeshRenderer m_IndicatorMesh;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (this.IsComplete())
+        {
+            m_IndicatorMesh.sharedMaterial = m_CompleteMaterial;
+        }
+        else
+        {
+            m_IndicatorMesh.sharedMaterial = m_IncompleteMaterial;
+        }
+    }
+
+    public bool IsComplete()
+    {
+        return false;
+    }
+}
