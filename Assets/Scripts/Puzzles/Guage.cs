e1ff32fceaefe08a7e5b9bb0064531c24fd2f6f7
dhog10 <dhog10@googlemail.com>
2020-10-04 14:43:06 +0100

    stuff

---
+++ Assets/Scripts/Puzzles/Guage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guage : MonoBehaviour
+{
+    public SteamPipe[] m_Pipes;
+
+    public float Pressure {
+        get {
+            var numPipes = m_Pipes.Length;
+            var numSealed = 0;
+
+            foreach (var pipe in m_Pipes)
+            {
+                if (!pipe.Leaking)
+                {
+                    numSealed++;
+                }
+            }
+
+            return numSealed / numPipes;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Usables/SteamPipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamPipe : MonoBehaviour
+{
+    private bool m_Leaking = true;
+
+    public bool Leaking
+        => m_Leaking;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Seal()
+    {
+        m_Leaking = false;
+    }
+}
