8c7947d21b2201755823ce8e0d3f0271ec4c0800
Luke Dicken - Jones <32326849+Luke-DJ@users.noreply.github.com>
2020-10-05 00:47:03 +0100

    Added beginnings of countdown functionality

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,36 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] private int m_CountdownDuration = 60;
+    [SerializeField] private TextMeshPro m_Text;
+    private int m_CurrentTime;
+
+    private void Start()
+        => StartCoroutine(this.CountdownRoutine());
+
+    private IEnumerator CountdownRoutine()
+    {
+        m_CurrentTime = m_CountdownDuration;
+
+        while (m_CurrentTime > 0)
+        {
+            m_Text?.SetText(m_CurrentTime.ToString("00"));
+            yield return new WaitForSeconds(1f);
+            m_CurrentTime--;
+        }
+        m_Text?.SetText(m_CurrentTime.ToString("00"));
+    }
+
+    public void AddToCountdown(int countdownIncrease)
+        => m_CurrentTime += countdownIncrease;
+
+    public void RemoveFromCountdown(int countdownDecrease)
+        => m_CurrentTime -= countdownDecrease;
+}
+
+#pragma warning restore 0649
